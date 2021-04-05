31ddfaa639a3e3743baf1e103cfd6b94d4397ce9
Chuck Wang <chuck.wang@spark.co.nz>
2021-04-05 15:16:38 +1200

    Initial Commit

---
+++ TransactionListDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TransactionListDemo
+//
+//  Created by XUN WANG on 5/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
